b4967e478abd23008f09a7fbcd5ce4784b43e47f
Chalie <c8455624156@163.com>
2025-08-08 15:28:58 +0800

    modified utils

--- utils/split_dataset.py
+++ utils/split_dataset.py
@@ -5,13 +5,18 @@ from tqdm import tqdm
 
 TOKENSET_PATH = '/root/autodl-tmp/dataset'
 
-# 下载预训练数据集
-os.system(f"modelscope download --dataset ddzhu123/seq-monkey mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2 --local_dir {TOKENSET_PATH}")
-# 解压预训练数据集
-os.system(f"tar -xvf {TOKENSET_PATH}/mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2")
+# # 下载预训练数据集
+# os.system(f"modelscope download --dataset ddzhu123/seq-monkey mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2 --local_dir {TOKENSET_PATH}")
+# # 解压预训练数据集
+# os.system(f"tar -xvf {TOKENSET_PATH}/mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2 -C {TOKENSET_PATH}")
 
-# 下载SFT数据集
-os.system(f'hf download --repo-type dataset --resume-download BelleGroup/train_3.5M_CN --local-dir {TOKENSET_PATH}/BelleGroup')
+# # 下载SFT数据集
+# os.system(f'hf download --repo-type dataset BelleGroup/train_3.5M_CN --local-dir {TOKENSET_PATH}/BelleGroup')
+
+# # 下载SFT数据集（改为 ModelScope）
+# os.system(f"modelscope download --dataset swift/train_3.5M_CN --local_dir {TOKENSET_PATH}/swift")
+
+
 
 
 
@@ -20,7 +25,7 @@ def split_text(text, chunk_size=512):
     """将文本按指定长度切分成块"""
     return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
 
-input_file = 'mobvoi_seq_monkey_general_open_corpus.jsonl'
+input_file = '/root/autodl-tmp/dataset/mobvoi_seq_monkey_general_open_corpus.jsonl'
 
 with open('seq_monkey_datawhale.jsonl', 'a', encoding='utf-8') as pretrain:
     with open(input_file, 'r', encoding='utf-8') as f:
@@ -49,9 +54,10 @@ def convert_message(data):
     return message
 
 with open('BelleGroup_sft.jsonl', 'a', encoding='utf-8') as sft:
-    with open('BelleGroup/train_3.5M_CN.json', 'r', encoding='utf-8') as f:
+    with open(f"{TOKENSET_PATH}/swift/train_3.5M_CN.json", 'r', encoding='utf-8') as f:
         data = f.readlines()
         for item in tqdm(data, desc="Processing", unit="lines"):
             item = json.loads(item)
             message = convert_message(item['conversations'])
             sft.write(json.dumps(message, ensure_ascii=False) + '\n')
+
